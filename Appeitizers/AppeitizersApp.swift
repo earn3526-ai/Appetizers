81d4865cfdd25e5deac589e7912a636c785cd4fd
Nilaksh <nilaksh@initfusion.in>
2025-09-01 14:25:51 +0530

    Initial Commit

---
+++ Appeitizers/AppeitizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppeitizersApp.swift
+//  Appeitizers
+//
+//  Created by Pavan on 01/09/25.
+//
+
+import SwiftUI
+
+@main
+struct AppeitizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
