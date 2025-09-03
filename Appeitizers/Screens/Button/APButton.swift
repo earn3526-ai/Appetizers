571d1939af1ec918ef5cb22f5e9f809e641c8c46
Nilaksh <nilaksh@initfusion.in>
2025-09-03 17:56:14 +0530

    refactor

---
+++ Appeitizers/Screens/Button/APButton.swift
@@ -0,0 +1,18 @@
+//
+//  APButton.swift
+//  Appeitizers
+//
+//  Created by Pavan on 03/09/25.
+//
+
+import SwiftUI
+
+struct APButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    APButton()
+}
---
+++ Appeitizers/Screens/Button/XDismissButton.swift
@@ -0,0 +1,18 @@
+//
+//  XDismissButton.swift
+//  Appeitizers
+//
+//  Created by Pavan on 03/09/25.
+//
+
+import SwiftUI
+
+struct XDismissButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    XDismissButton()
+}
