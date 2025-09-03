4efb296f5f2bd93f9a7407527cc4a514bee4f67d
Nilaksh <nilaksh@initfusion.in>
2025-09-03 15:35:55 +0530

    detail view

---
+++ Appeitizers/Screens/AppeitizerView/DetailsCardView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsCardView.swift
+//  Appeitizers
+//
+//  Created by Pavan on 03/09/25.
+//
+
+import SwiftUI
+
+struct DetailsCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsCardView()
+}
---
+++ Appeitizers/Utlities/Manager/NetworkManager.swift
@@ -0,0 +1,57 @@
+//
+//  NetworkManager.swift
+//  Appeitizers
+//
+//  Created by Pavan on 02/09/25.
+//
+
+import Foundation
+
+
+final class NetworkManager{
+    
+    static let shared = NetworkManager()
+    static let baseUrl = "https://dummyjson.com/"
+     let appeitzerUrl = baseUrl + "recipes"
+    private init(){
+        
+    }
+    
+    func getAppietizers(parms: String = "" , completion: @escaping (Result<[Appeitizer], APError>) -> Void){
+        guard let url = URL(string: appeitzerUrl) else{
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data,response, error in
+            if let _ = error{
+                completion(.failure(.unableToComplete))
+                return
+            }
+            guard let  response = response as? HTTPURLResponse, response.statusCode == 200 else{
+                completion(.failure(.invalidResponse))
+                return
+            }
+            guard let data = data else{
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do{
+                let decoder = JSONDecoder()
+                let decodedResponse = try decoder.decode(AppeitzerResponse.self, from: data)
+                if let json = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers),
+                   let jsonData = try? JSONSerialization.data(withJSONObject: json, options: .prettyPrinted) {
+                    print(String(decoding: jsonData, as: UTF8.self))
+                } 
+                completion(.success(decodedResponse.recipes))
+            }catch{
+                completion(.failure(.invalidData))
+            }
+            
+        }
+        task.resume()
+    }
+    
+}
+
